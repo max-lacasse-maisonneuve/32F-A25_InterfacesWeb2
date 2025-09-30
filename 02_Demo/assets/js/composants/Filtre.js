31068ccd060e7b60c2feeca807d455e6d70a3c75
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2025-09-30 09:07:14 -0400

    ajout filtre

--- 02_Demo/assets/js/composants/Filtre.js
+++ 02_Demo/assets/js/composants/Filtre.js
@@ -1,5 +1,26 @@
 class Filtre {
-    constructor() {}
+    #conteneurHTML;
+    #elementHTML;
+
+    constructor(conteneurHTML) {
+        this.#conteneurHTML = conteneurHTML;
+    }
+
+    injecterHTML() {
+        const gabarit = `
+            <div class="filtres" data-action="tri">
+                <div class="filtre" data-categorie="prix" data-direction="asc">Prix ascendant</div>
+                <div class="filtre" data-categorie="prix" data-direction="desc">Prix descendant</div>
+            </div>`;
+        this.#conteneurHTML.insertAdjacentHTML("beforeend", gabarit);
+
+        this.#elementHTML = this.#conteneurHTML.lastElementChild;
+        this.#elementHTML.addEventListener("click", this.clicTri.bind(this));
+    }
+
+    clicTri(evenement) {}
+
+    trierParPrix(direction) {}
 }
 
 export default Filtre;
